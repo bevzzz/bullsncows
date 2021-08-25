3acb84f2cd5e6b136865247951a794c3c9cacba9
bevzzz <dmytro.y.solovei@gmail.com>
2021-08-25 09:17:48 +0200

    refactor: delegate creation of new game to crud functions

--- src/server/player.py
+++ src/server/player.py
@@ -19,16 +19,8 @@ class Player:
         self.mode = mode
         self.games = {}
 
-    def start_new_game(self) -> uuid.UUID:
-        # create a new game
-        # @return: id of the new game
-        new_id = uuid.uuid4()
-        number = self._new_number()
-        self.games[new_id] = number
-        return new_id
-
     @staticmethod
-    def _new_number():
+    def new_number():
         nums = random.sample(range(10), 4)
         s = [str(n) for n in nums]
         return "".join(s)
