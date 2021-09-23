dc969088a0bd9f32e4bba435ab7f93475116432f
bevzzz <dmytro.y.solovei@gmail.com>
2021-09-23 10:20:11 +0200

    chore: use the new function fromm the `db` module to satisfy the db dependency
    format the code for better readability

--- src/server/app/api.py
+++ src/server/app/api.py
@@ -10,28 +10,24 @@ from fastapi import FastAPI, Header, Body, Depends, status, HTTPException
 import src.server.app.headers as headers
 from src.server.app import schemas
 from src.server.db import crud, model
-from src.server.db.db import SessionLocal, engine
+from src.server.db.db import engine, start_new_db_session
 from src.server.player import Player, MODE
 
-
-app = FastAPI()
-computer = Player(mode=MODE.generate)
 
 # Inject dependencies
 # create the tables (should be done with Alembic)
 model.Base.metadata.create_all(bind=engine)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app = FastAPI()
+computer = Player(mode=MODE.generate)
 
 
 @app.post("/game/new/{user_id}", status_code=status.HTTP_201_CREATED)
-def new_game(user_id: int, authorization: Optional[str] = Header(None), db: orm.Session = Depends(get_db)):
+def new_game(
+        user_id: int,
+        authorization: Optional[str] = Header(None),
+        db: orm.Session = Depends(start_new_db_session)
+):
     """
     Start a new game
     :param user_id: internal user ID
@@ -53,7 +49,11 @@ def new_game(user_id: int, authorization: Optional[str] = Header(None), db: orm.
 
 
 @app.get("/game/guess/{game_id}", response_model=schemas.Score, status_code=status.HTTP_200_OK)
-def get_score(game_id: uuid.UUID, number: str = Body(..., embed=True), db: orm.Session = Depends(get_db)):
+def get_score(
+        game_id: uuid.UUID,
+        number: str = Body(..., embed=True),
+        db: orm.Session = Depends(start_new_db_session)
+):
     """
     Make a guess at the number
     :param game_id: the game being played
