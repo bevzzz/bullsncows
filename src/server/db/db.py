1f1ef1b353cd96381cd87a2dd1f23605ae50cbab
bevzzz <dmytro.y.solovei@gmail.com>
2021-09-25 16:36:17 +0200

    chore: store database url as an environment variable

--- src/server/db/db.py
+++ src/server/db/db.py
@@ -1,12 +1,12 @@
 # Third-party libraries
+import decouple
 import sqlalchemy as sql
 import sqlalchemy.orm as orm
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bullcow.db"
 
 engine = sql.create_engine(
-    url=SQLALCHEMY_DATABASE_URL,
+    url=decouple.config("SQLALCHEMY_DATABASE_URL"),
     connect_args={"check_same_thread": False}
 )
 
